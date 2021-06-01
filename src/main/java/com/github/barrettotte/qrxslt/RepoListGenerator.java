ce0f53b1fc976a793210dd1afd9716c480dcdd6c
barrettotte <barrettotte@gmail.com>
2021-06-01 11:12:12 -0400

    add todo items

--- src/main/java/com/github/barrettotte/qrxslt/RepoListGenerator.java
+++ src/main/java/com/github/barrettotte/qrxslt/RepoListGenerator.java
@@ -4,7 +4,10 @@ public class RepoListGenerator {
 
     public static void main(String[] args) {
         try {
-            // TODO:
+            // TODO: make test data XML
+            // TODO: generate basic PDF w/o QR Code
+            // TODO: generate basic QR code
+            // TODO: hit GitHub API for live data XML
             System.out.println("Hello");
         } catch(final Exception e) {
             e.printStackTrace();
