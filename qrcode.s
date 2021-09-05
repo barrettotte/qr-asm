8bdbcd3798157ea70772e8b1540a32dac8e338a6
barrettotte <barrettotte@gmail.com>
2021-09-05 16:10:35 -0400

    timing patterns done

--- qrcode.s
+++ qrcode.s
@@ -13,8 +13,7 @@
             // internal:
             //
             // add_square   - add a square to matrix
-            // add_time_h   - add horizontal timing to matrix
-            // add_time_v   - add vertical timing to matrix
+            // add_timepats - add horizontal and vertical timing patterns
             // add_finder   - add a finder to matrix
             // add_aligns   - add alignment patterns and dark module to matrix
 
@@ -83,6 +82,41 @@ _asq_x_next:
             pop   {r4-r11, lr}             // restore caller's vars + return address
             bx    lr                       // return from subroutine
 
+add_timepats:                              // ***** Add timing patterns to QR matrix *****
+                                           // r0 - pointer to QR matrix
+                                           // r1 - unused; clobbered
+                                           // r2 - QR width
+                                           // r3 - QR version
+            push  {r4-r11, lr}             // save caller's vars + return address
+
+            mov   r4, #1                   // is_dark = true
+            mov   r5, #0                   // i = 0
+            mov   r7, #6                   // timing offset
+_at_tpat_loop:                             //
+            
+            cmp   r4, #1                   // is_dark?
+            beq   _at_tpat_dark            // use dark module
+            mov   r6, $MOD_RLT             // set light module ASCII
+            b     _at_tpat_mod             // skip over next 2 lines
+_at_tpat_dark:
+            mov   r6, $MOD_RDK             // set dark module ASCII
+_at_tpat_mod:
+            umull r9, r10, r2, r5          // x = i * qr_size
+            add   r9, r9, r7               // x = (i * qr_size) + offset
+            strb  r6, [r0, r9]             // qr_mat[x] = r6; horizontal timing
+
+            umull r9, r10, r7, r2          // x = offset * qr_size
+            add   r9, r9, r5               // x = (offset * qr_size) + i
+            strb  r6, [r0, r9]             // qr_mat[x] = r6; vertical timing
+_at_tpat_next:
+            eor   r4, r4, #1               // is_dark = !is_dark
+            add   r5, r5, #1               // i++
+            cmp   r5, r2                   // check loop condition
+            blt   _at_tpat_loop            // while (i < qr_width)
+
+            pop   {r4-r11, lr}             // restore caller's vars + return address
+            bx    lr                       // return from subroutine
+
 qr_reserved:                               // ***** Add reserved areas to QR matrix *****
                                            // r0 - pointer to QR matrix
                                            // r1 - unused; clobbered
@@ -93,14 +127,13 @@ qr_reserved:                               // ***** Add reserved areas to QR mat
             mov   r5, #9                   // set square width
             mov   r6, #MOD_RLT             // set fill byte
             mov   r1, #0                   // reset unused arg
-_rsquare_tl:                               // set top left reserved square
+
             mov   r4, #0                   // set x,y; sq_args[3] = 0, sq_args[2] = 0
             orr   r1, r1, r5, lsl #8       // set square width; sq_args[1]
             orr   r1, r1, r6               // set fill byte; sq_args[0]
             bl    add_square               // add top left square to QR matrix
             eor   r1, r1, r1               // reset sq_args
 
-_rsquare_tr:                               // set top right reserved square
             sub   r4, r2, r5               // x = qr_size - 9
             orr   r1, r1, r4, lsl #24      // set x position
             orr   r1, r1, r5, lsl #8       // set square width
@@ -108,12 +141,13 @@ _rsquare_tr:                               // set top right reserved square
             bl    add_square               // add top right square to QR matrix
             eor   r1, r1, r1               // reset sq_args
 
-_rsquare_bl:                               // set bottom left reserved square
             sub   r4, r2, r5               // y = qr_size - 9
             orr   r1, r1, r4, lsl #16      // set y position
             orr   r1, r1, r5, lsl #8       // set square width
             orr   r1, r1, r6               // set fill byte
             bl    add_square               // add bottom left square to QR matrix
+
+            bl    add_timepats             // add horizontal and vertical timing patterns
 
             pop   {r4-r11, lr}             // restore caller's vars + return address
             bx    lr                       // return from subroutine
